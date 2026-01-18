ea9714c7552bdb9f36340ce95c9095c29c2fa0c9
hidnt <mishboldi@yandex.ru>
2026-01-18 20:59:43 +0300

    commit

---
+++ files/scripts/build-nvidia.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -ouex pipefail
+
+echo "Building nvidia proprietary drivers"
+
+cd /etc/yum.repos.d/
+
+rpm-ostree install \
+    gcc \
+    gcc-c++ \
+    make \
+    automake \
+    akmod-nvidia
+
+KERNEL_VERSION=$(rpm -qa kernel-cachyos-core | sed 's/kernel-cachyos-core-//')
+if ! akmods --force --kernels "$KERNEL_VERSION" --rebuild; then
+    cat /var/cache/akmods/nvidia/*.failed.log
+    echo "Failed to build drivers"
+    exit 1
+fi
+
+echo "Nvidia driver build completed"
---
+++ files/scripts/change-kernel.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -ouex pipefail
+
+echo "Setting up CachyOS kernel"
+
+cd /etc/yum.repos.d/
+wget https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos/repo/fedora-$(rpm -E %fedora)/bieszczaders-kernel-cachyos-fedora-$(rpm -E %fedora).repo
+
+rpm-ostree override remove \
+    kernel \
+    kernel-core \
+    kernel-modules \
+    kernel-modules-core \
+    kernel-modules-extra \
+    --install kernel-headers \
+    --install kernel-cachyos \
+    --install kernel-cachyos-devel-matched
+
+echo "CachyOS kernel setup completed"
